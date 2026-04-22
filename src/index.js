9bbc22eed3777606bbb4d82489350e38f63c7d2c
Pushpamchy <144523178+Pushpamchy@users.noreply.github.com>
2026-04-22 15:11:36 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GreencoreDashboard from "./GreencoreDashboard";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<GreencoreDashboard />);
